c591fa68993f4a115998d170920609db4fa5cabb
Xiehd <xiehdwork@163.com>
2022-06-23 14:18:18 +0800

    项目搭建

---
+++ src/main/java/com/ahzx/baohanapi/GuaranteeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ahzx.baohanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaranteeDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaranteeDataServiceApplication.class, args);
+    }
+
+}
