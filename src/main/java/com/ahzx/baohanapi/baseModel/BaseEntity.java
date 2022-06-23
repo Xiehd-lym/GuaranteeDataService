e349a56a1eba4176989f33281c11d7732a62e6e2
Xiehd <xiehdwork@163.com>
2022-06-23 15:25:38 +0800

    逆向工程前保存版本

---
+++ src/main/java/com/ahzx/baohanapi/baseModel/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.ahzx.baohanapi.baseModel;
+
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+/**
+ * @Author xiehd
+ * @Date 2022 06 23
+ **/
+@Data
+//@Accessors用于配置getter和setter方法的生成结果,chain设置为true，则setter方法返回当前对象
+@Accessors(chain = true)
+public class BaseEntity {
+    @ApiModelProperty(value = "ID")
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private String id;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @ApiModelProperty(value = "逻辑删除（0 未删除、1 删除）")
+    @TableLogic(value = "0", delval = "1")
+    @TableField(fill = FieldFill.INSERT)
+    private Integer deleteFlag;
+
+}
