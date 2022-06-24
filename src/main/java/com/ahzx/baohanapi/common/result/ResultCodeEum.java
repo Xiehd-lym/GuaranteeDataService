974003fe4c774d39d95d070a06f913990c644876
dt <1058240992@qq.com>
2022-06-24 11:35:47 +0800

    SignUtils完善

--- src/main/java/com/ahzx/baohanapi/common/result/ResultCodeEum.java
+++ src/main/java/com/ahzx/baohanapi/common/result/ResultCodeEum.java
@@ -10,8 +10,19 @@ public enum ResultCodeEum {
     SUCCESS(true,200,"成功"),
     ERROR(false,500,"失败"),
 
+    UNKOWN_REASON(false,20001,"未知错误"),
 
-    ;
+    BAD_SQL_GRAMMAR(false,21001,"sql语法错误"),
+    JSON_PARSE_ERROR(false,21002,"json解析异常"),
+    PARAM_ERROR(false,21003,"参数不正确"),
+
+    URL_ENCODE_ERROR(false,22001,"URL编码失败"),
+    ILLEGAL_CALLBACK_REQUEST_ERROR(false,22002,"非法回调请求"),
+    FETCH_USERINFO_ERROR(false,22003,"获取用户信息失败"),
+
+    GATEWAY_ERROR(false,23001,"服务不能访问"),
+    PHONE_ERROR(false,23002,"手机号不正确"),
+    ACCOUNT_ERROR(false,23003,"账号不正确");
 
     private Boolean success;
     private Integer code;
--- src/main/java/com/ahzx/baohanapi/common/util/SignUtils.java
+++ src/main/java/com/ahzx/baohanapi/common/util/SignUtils.java
@@ -1,7 +1,55 @@
 package com.ahzx.baohanapi.common.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class SignUtils {
+
+    private static String appId;
+
+    private static String accessKeyId;
+
+    private static String keySecret;
+
+    @Value("${sign.appid}")
+    public void setAppid(String appId) {
+        this.appId = appId;
+    }
+
+    @Value("${sign.accessKeyId}")
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+
+    @Value("${sign.keySecret}")
+    public void setKeySecret(String keySecret) {
+        this.keySecret = keySecret;
+    }
+
+    // signature签名生成（SM3加密）
+    public static String signature() {
+
+        SignUtils signUtils = new SignUtils();
+        String timestamp = signUtils.getTimestamp();
+
+        String uuid = UUID.randomUUID().toString().trim().replace("-", "");
+
+        String baseString = "accessKeyId=" + accessKeyId + "&appId=" + appId +
+                "&timestamp=" + timestamp + "&uuid=" + uuid;
+
+        return cn.hutool.crypto.SmUtil.sm3(keySecret + baseString);
+    }
+
+    public String getTimestamp() {
+        // 生成时间戳
+        Long timestampLong = System.currentTimeMillis();
+
+        String timestampStr = String.valueOf(timestampLong);
+
+        return timestampStr;
+    }
+
 }
