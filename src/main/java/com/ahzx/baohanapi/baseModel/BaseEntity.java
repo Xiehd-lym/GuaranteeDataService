497fb7a6df6767687d109c14c7f413253bb321c2
Xiehd <xiehdwork@163.com>
2022-06-25 18:12:48 +0800

    统一sql数据库结构

--- src/main/java/com/ahzx/baohanapi/baseModel/BaseEntity.java
+++ src/main/java/com/ahzx/baohanapi/baseModel/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.ahzx.baohanapi.baseModel;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -13,7 +16,7 @@ import java.util.Date;
 @Data
 //@Accessors用于配置getter和setter方法的生成结果,chain设置为true，则setter方法返回当前对象
 @Accessors(chain = true)
-public class BaseEntity {
+public class BaseEntity{
     @ApiModelProperty(value = "ID")
     @TableId(value = "id", type = IdType.AUTO)
     private String id;
